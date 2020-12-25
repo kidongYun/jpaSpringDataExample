525397b608346092feb61242699682b32b710fd7
Kidong Yun <oriondigestive@gmail.com>
2020-12-25 22:30:20 +0900

    SPRING DATA JPA

--- src/main/java/me/whiteship/demojpaspringdata2/PostRepository.java
+++ src/main/java/me/whiteship/demojpaspringdata2/PostRepository.java
@@ -1,7 +1,12 @@
 package me.whiteship.demojpaspringdata2;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+        import org.springframework.data.domain.Pageable;
+        import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 }
