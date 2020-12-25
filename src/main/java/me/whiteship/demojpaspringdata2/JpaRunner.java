c009da0038200598ac414089ea783297cbb1d617
Kidong Yun <oriondigestive@gmail.com>
2020-12-25 15:36:31 +0900

    SPRING DATA JPA

--- src/main/java/me/whiteship/demojpaspringdata2/JpaRunner.java
+++ src/main/java/me/whiteship/demojpaspringdata2/JpaRunner.java
@@ -18,24 +18,27 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Account account = new Account();
-        account.setUsername("kidongyun");
-        account.setPassword("hibernate");
-
-        Study study = new Study();
-        study.setName("Spring Data JPA");
-
-        account.addStudy(study);
+//        Post post = new Post();
+//        post.setTitle("Spring Data JPA 언제 보나...");
+//
+//        Comment comment = new Comment();
+//        comment.setComment("빨리 보고 싶어요.");
+//        post.addComment(comment);
+//
+//        Comment comment1 = new Comment();
+//        comment1.setComment("곧 보여드릴게요.");
+//        post.addComment(comment1);
 
         Session session = entityManager.unwrap(Session.class);
-        session.save(account);
-        session.save(study);
+//        session.save(post);
 
-        Account keesun = session.load(Account.class, account.getId());
-        keesun.setUsername("whiteship");
-        keesun.setUsername("helloworld");
-        keesun.setUsername("kidongyun");
-        System.out.println("============================");
-        System.out.println(keesun.getUsername());
+        Post post = session.get(Post.class, 1L);
+        System.out.println("=========================");
+        System.out.println(post.getTitle());
+
+        post.getComments().forEach(c -> {
+            System.out.println("------------");
+            System.out.println(c.getComment());
+        });
     }
 }
